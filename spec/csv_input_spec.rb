6e147820727f5d14461fbf6e10dd29856ac10582
Bruce Tesar <tesar@rutgers.edu>
2021-02-19 15:28:32 -0500

    Added tests to csv_input_spec.rb to verify that numeric data in CSV files are returned as strings by CsvInput.

--- spec/csv_input_spec.rb
+++ spec/csv_input_spec.rb
@@ -21,4 +21,17 @@ RSpec.describe 'CsvInput' do
                                      ['E2', 'e', 'W', 'L']])
     end
   end
+
+  context 'when created with a file containing numeric data' do
+    before(:example) do
+      infile = File.join(fixture_dir, 'cand_input1.csv')
+      @csv_input = CsvInput.new(infile)
+    end
+    it 'returns a data array of all strings' do
+      expect(@csv_input.data[0]).to all be_a_kind_of(String)
+    end
+    it 'returns an array of arrays of the data' do
+      expect(@csv_input.data).to eq([['in1', 'out11', '0', '3', '2']])
+    end
+  end
 end
