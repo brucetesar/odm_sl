4d94ff92a7ad9c373fbd8b2db4251e11d4b36366
Bruce Tesar <tesar@rutgers.edu>
2021-02-20 16:20:05 -0500

    Constructed specs and implementation for OTGeneric::WinnersReader, which converts a CSV-style array of input/output pairs into a list of candidates, using a pre-existing list of competitions (containing complete candidates).

---
+++ lib/otgeneric/winners_reader.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+module OTGeneric
+  # Contains methods for converting a list of winners from an array
+  # of strings for input and output to an array of candidate objects.
+  # The candidate objects are retrieved from the externally supplied
+  # attribute _competitions_.
+  class WinnersReader
+
+    # The list of competitions, each containing a list of candidates.
+    attr_accessor :competitions
+
+    # Returns a new WinnersReader object.
+    def initialize
+      @competitions = []
+    end
+
+    # For each pair of input/output strings in _data_,
+    # the corresponding candidate is retrieved from the competitions
+    # and added to the list of winners.
+    # Returns an array of Candidate objects.
+    def convert_array_to_winners(data)
+      winner_list = []
+      data.each do |row|
+        comp = find_competition_for_input(row[0], @competitions)
+        winner = find_candidate_for_output(row[1], comp)
+        winner_list << winner
+      end
+      winner_list
+    end
+
+    # Find the competition corresponding to the _input_.
+    # Return the found competition.
+    # If _competitions_ does not contain a competition for _input_,
+    # raise an exception.
+    def find_competition_for_input(input, competitions)
+      match_comp = competitions.find {|comp| comp[0].input == input}
+      if match_comp.nil?
+        msg = "Winner has input #{input}, but there is no such competition."
+        raise msg
+      end
+      match_comp
+    end
+
+    # Find the candidate corresponding to the _output_.
+    # Return the candidate.
+    # If _competition_ does not contain a candidate for _output_,
+    # raise an exception.
+    def find_candidate_for_output(output, competition)
+      winner = competition.find {|cand| cand.output == output}
+      if winner.nil?
+        msg1 = "Winner has input #{competition[0].input} output #{output},"
+        msg2 = "but there is no such candidate."
+        raise "#{msg1} #{msg2}"
+      end
+      winner
+    end
+  end
+end
---
+++ spec/otgeneric/winners_reader_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+require 'rspec'
+require 'otgeneric/winners_reader'
+
+RSpec.describe 'OTGeneric::WinnersReader' do
+  let(:cand11) { double('candidate11')}
+  let(:cand12) { double('candidate12')}
+  let(:cand21) { double('candidate21')}
+  let(:cand22) { double('candidate22')}
+  before(:example) do
+    allow(cand11).to receive(:input).and_return('in1')
+    allow(cand12).to receive(:input).and_return('in1')
+    allow(cand21).to receive(:input).and_return('in2')
+    allow(cand22).to receive(:input).and_return('in2')
+    allow(cand11).to receive(:output).and_return('out11')
+    allow(cand12).to receive(:output).and_return('out12')
+    allow(cand21).to receive(:output).and_return('out21')
+    allow(cand22).to receive(:output).and_return('out22')
+  end
+  context 'given an array with one winner' do
+    before(:example) do
+      data = [['in1', 'out11']]
+      comp_list = [[cand11, cand12], [cand21, cand22]]
+      @win_reader = OTGeneric::WinnersReader.new
+      @win_reader.competitions = comp_list
+      @winner_list = @win_reader.convert_array_to_winners(data)
+    end
+    it 'returns an array with one winning candidate' do
+      expect(@winner_list.size).to eq 1
+    end
+    it 'returns an array including a candidate with input in1' do
+      expect(@winner_list[0].input).to eq 'in1'
+    end
+    it 'returns an array including a candidate with output out11' do
+      expect(@winner_list[0].output).to eq 'out11'
+    end
+  end
+  context 'given an array with two winners' do
+    before(:example) do
+      data = [['in1', 'out12'], ['in2', 'out21']]
+      comp_list = [[cand11, cand12], [cand21, cand22]]
+      @win_reader = OTGeneric::WinnersReader.new
+      @win_reader.competitions = comp_list
+      @winner_list = @win_reader.convert_array_to_winners(data)
+    end
+    it 'returns an array with two winning candidates' do
+      expect(@winner_list.size).to eq 2
+    end
+    it 'returns an array including a candidate with input in1' do
+      expect(@winner_list[0].input).to eq 'in1'
+    end
+    it 'returns an array including a candidate with output out12' do
+      expect(@winner_list[0].output).to eq 'out12'
+    end
+    it 'returns an array including a candidate with input in2' do
+      expect(@winner_list[1].input).to eq 'in2'
+    end
+    it 'returns an array including a candidate with output out21' do
+      expect(@winner_list[1].output).to eq 'out21'
+    end
+  end
+  context 'given a winner with input with no corresponding competition' do
+    before(:example) do
+      @data = [['in1', 'out12'], ['in3', 'out31']]
+      comp_list = [[cand11, cand12], [cand21, cand22]]
+      @win_reader = OTGeneric::WinnersReader.new
+      @win_reader.competitions = comp_list
+    end
+    it 'raises a RuntimeError' do
+      msg = "Winner has input in3, but there is no such competition."
+      expect {@win_reader.convert_array_to_winners(@data)}.to\
+        raise_error(RuntimeError, msg)
+    end
+  end
+  context 'given a winner with no output in its competition' do
+    before(:example) do
+      @data = [['in1', 'out12'], ['in2', 'out31']]
+      comp_list = [[cand11, cand12], [cand21, cand22]]
+      @win_reader = OTGeneric::WinnersReader.new
+      @win_reader.competitions = comp_list
+    end
+    it 'raises a RuntimeError' do
+      msg = "Winner has input in2 output out31, but there is no such candidate."
+      expect {@win_reader.convert_array_to_winners(@data)}.to\
+        raise_error(RuntimeError, msg)
+    end
+  end
+end
