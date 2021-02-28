2f87e35da1dbe0ad2982fba0db3adb7ba62c243f
Bruce Tesar <tesar@rutgers.edu>
2021-02-28 03:36:38 -0500

    Created script dos2mac.rb, which copies selected parts of the project tree odm_sl to a parallel tree odm_sl_mac, and transcodes all of the files to use the MacOS line terminator code.

---
+++ bin/dos2mac.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+# Write a parallel project directory in which all of the text
+# file have been converted to MacOS-style format.
+# DOS: CR+LF; nothing at end of file.
+# MacOS: CR; CR needed at end of file.
+
+require_relative '../lib/odl/resolver'
+
+require 'find'
+
+# Takes the string _str_, converts it to the universal newline
+# convention (regardless of what newline convention it originally was),
+# and then converts it to the MacOS newline convention (CR only).
+def string2mac(str)
+  univ_str = str.encode(str.encoding, universal_newline: true)
+  univ_str.encode(str.encoding, cr_newline: true)
+end
+
+# Define source and target project directories
+SOURCE = ODL::PROJECT_DIR
+target_relative_path = File.join(SOURCE, '../odm_sl_mac')
+TARGET = File.expand_path(target_relative_path)
+Dir.mkdir TARGET unless Dir.exist? TARGET
+
+# Traverse source tree
+Find.find(SOURCE) do |path|
+  Find.prune if File.basename(path) == '.git'
+  Find.prune if File.basename(path) == '.idea'
+  Find.prune if File.basename(path) == 'data'
+  Find.prune if File.basename(path) == 'doc'
+  Find.prune if File.basename(path) == 'features'
+  Find.prune if File.basename(path) == 'spec'
+  Find.prune if File.basename(path) == 'temp'
+  Find.prune if File.basename(path) == 'test'
+  Find.prune if File.basename(path) == 'tmp'
+  Find.prune if File.basename(path) == '.gitignore'
+  Find.prune if File.basename(path) == '.rspec'
+  Find.prune if File.basename(path) == 'LICENSE'
+  Find.prune if File.basename(path) == 'README'
+  Find.prune if File.basename(path) == 'Rakefile'
+  # Translate the path to the target
+  target_path = path.gsub(SOURCE, TARGET)
+  if File.directory?(path)
+    # If a directory, a target counterpart exists.
+    Dir.mkdir(target_path) unless Dir.exist?(target_path)
+  else
+    # If the path is not a directory, then it must be a file.
+    # Read the file in binary mode into an ASCII-8BIT string.
+    content_string = IO.read(path, mode: 'rb')
+    # Transcode the string into MacOS line terminator format.
+    trans_string = string2mac(content_string)
+    # Write the transcoded string to the target directory.
+    IO.write(target_path, trans_string, mode: 'wb')
+  end
+end
