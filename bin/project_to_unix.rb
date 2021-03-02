dfe4cf6686e04bcdd8667a4f582d34e53b167163
Bruce Tesar <tesar@rutgers.edu>
2021-03-01 19:56:11 -0500

    Changed the program converting line endings from Mac format (outdated) to unix format (LF only). Renamed the code project_to_unix.rb.

--- bin/project_to_unix.rb
+++ bin/project_to_unix.rb
@@ -3,9 +3,9 @@
 # Author: Bruce Tesar
 
 # Write a parallel project directory in which all of the text
-# file have been converted to MacOS-style format.
+# files have been converted to unix-style format.
 # DOS: CR+LF; nothing at end of file.
-# MacOS: CR; CR needed at end of file.
+# Unix: LF; LF needed at end of file.
 
 require_relative '../lib/odl/resolver'
 
@@ -13,17 +13,22 @@ require 'find'
 
 # Takes the string _str_, converts it to the universal newline
 # convention (regardless of what newline convention it originally was),
-# and then converts it to the MacOS newline convention (CR only).
-def string2mac(str)
+# and then converts it to the unix newline convention (LF only).
+def string2unix(str)
   univ_str = str.encode(str.encoding, universal_newline: true)
-  univ_str.encode(str.encoding, cr_newline: true)
+  univ_str.encode(str.encoding, lf_newline: true)
 end
 
 # Define source and target project directories
 SOURCE = ODL::PROJECT_DIR
-target_relative_path = File.join(SOURCE, '../odm_sl_mac')
+target_relative_path = File.join(SOURCE, '../odm_sl_unix')
 TARGET = File.expand_path(target_relative_path)
 Dir.mkdir TARGET unless Dir.exist? TARGET
+
+# Delete any existing files in the target directory tree.
+Find.find(TARGET) do |path|
+  File.delete path if File.file?(path)
+end
 
 # Traverse source tree
 Find.find(SOURCE) do |path|
@@ -41,6 +46,7 @@ Find.find(SOURCE) do |path|
   Find.prune if File.basename(path) == 'LICENSE'
   Find.prune if File.basename(path) == 'README'
   Find.prune if File.basename(path) == 'Rakefile'
+  Find.prune if File.basename(path) =~ /[.]bat$/
   # Translate the path to the target
   target_path = path.gsub(SOURCE, TARGET)
   if File.directory?(path)
@@ -50,8 +56,8 @@ Find.find(SOURCE) do |path|
     # If the path is not a directory, then it must be a file.
     # Read the file in binary mode into an ASCII-8BIT string.
     content_string = IO.read(path, mode: 'rb')
-    # Transcode the string into MacOS line terminator format.
-    trans_string = string2mac(content_string)
+    # Transcode the string into unix line terminator format.
+    trans_string = string2unix(content_string)
     # Write the transcoded string to the target directory.
     IO.write(target_path, trans_string, mode: 'wb')
   end
