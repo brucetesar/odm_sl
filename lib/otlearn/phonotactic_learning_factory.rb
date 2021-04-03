a9cea9df630ca900e75b45a69e691e645bdd4f3a
Bruce Tesar <tesar@rutgers.edu>
2021-04-02 23:17:24 -0400

    Created a PhonotacticLearningFactory, for taking basic components and constructing full PhonotacticLearning objects.

---
+++ lib/otlearn/phonotactic_learning_factory.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+require 'loser_selector_from_gen'
+require 'otlearn/erc_learning'
+require 'otlearn/grammar_test'
+require 'otlearn/phonotactic_learning'
+
+module OTLearn
+  # A factory class for constructing phonotactic learner objects
+  # using required components. The required components must be
+  # provided via attribute assignment after the initial object
+  # has been constructed.
+  # === Required Components
+  # * system - a linguistic system object.
+  # * learning_comparer - the comparer (of candidates) used in
+  #   loser selection during actual phonotactic learning.
+  # * testing_comparer - the comparer (of candidates) used in
+  #   loser selection during grammar testing at the end of
+  #   phonotatic learning.
+  # === Build Outline
+  # The factory takes the components provided via the attributes
+  # and builds the intermediate components needed by
+  # OTLearn::PhonotacticLearning objects.
+  # * A learning loser selector, class LoserSelectorFromGen,
+  #   is created with _system_ and _learning_comparer_.
+  # * An ERC learner, class OTLearn::ErcLearning, is created with
+  #   the learning loser selector.
+  # * A testing loser selector, class LoserSelectorFromGen,
+  #   is created with _system_ and _testing_comparer_.
+  # * A grammar tester, class OTLearn::GrammarTest, is created with
+  #   the testing loser selector.
+  # * Finally, an OTLearn::PhonotacticLearning object is created with
+  #   the ERC learner and the grammar tester.
+  class PhonotacticLearningFactory
+    # The linguistic system
+    attr_accessor :system
+
+    # The comparer for learning
+    attr_accessor :learning_comparer
+
+    # The comparer for testing
+    attr_accessor :testing_comparer
+
+    # Returns a new PhonotacticLearningFactory object.
+    # :call-seq:
+    #   PhonotacticLearningFactory.new -> factory
+    def initialize
+      @system = nil
+      @learning_comparer = nil
+      @testing_comparer = nil
+    end
+
+    # TODO: standardize factories to use method name #build
+
+    # Returns an OTLearn::PhonotacticLearning object matching
+    # the factory-specified settings.
+    #
+    # Raises a RuntimeError if any of the required components
+    # have not been provided.
+    def build
+      check_factory_settings
+      learn_selector = create_loser_selector(@learning_comparer)
+      erc_learner = create_erc_learner(learn_selector)
+      test_selector = create_loser_selector(@testing_comparer)
+      tester = create_grammar_tester(test_selector)
+      ph_learner = PhonotacticLearning.new
+      ph_learner.erc_learner = erc_learner
+      ph_learner.grammar_tester = tester
+      ph_learner
+    end
+
+    # Checks that all required components are defined before
+    # building a phonotactic learner.
+    # Returns true if no errors occurred.
+    def check_factory_settings
+      msg1 = 'PhonotacticLearningFactory#build:'
+      # If no linguistic system has been specified, raise an error.
+      raise "#{msg1} no system specified." if system.nil?
+
+      # If no learning comparer has been specified, raise an error.
+      msg2 = 'no learning comparer specified.'
+      raise "#{msg1} #{msg2}" if learning_comparer.nil?
+
+      # If no testing comparer has been specified, raise an error.
+      msg2 = 'no testing comparer specified.'
+      raise "#{msg1} #{msg2}" if testing_comparer.nil?
+
+      true # return value if no errors raised
+    end
+    private :check_factory_settings
+
+    def create_loser_selector(comparer)
+      LoserSelectorFromGen.new(@system, comparer)
+    end
+    private :create_loser_selector
+
+    def create_erc_learner(selector)
+      erc_learner = OTLearn::ErcLearning.new
+      erc_learner.loser_selector = selector
+      erc_learner
+    end
+    private :create_erc_learner
+
+    def create_grammar_tester(selector)
+      tester = OTLearn::GrammarTest.new
+      tester.loser_selector = selector
+      tester
+    end
+    private :create_grammar_tester
+  end
+end
---
+++ spec/otlearn/phonotactic_learning_factory_spec.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+require 'rspec'
+require 'otlearn/phonotactic_learning_factory'
+
+RSpec.describe 'OTLearn::PhonotacticLearningFactory' do
+  let(:system) { double('system') }
+  let(:learning_comparer) { double('learning comparer') }
+  let(:testing_comparer) { double('testing comparer') }
+  before(:example) do
+    @factory = OTLearn::PhonotacticLearningFactory.new
+  end
+
+  context 'given a learning comparer and a testing comparer' do
+    before(:example) do
+      @factory.system = system
+      @factory.learning_comparer = learning_comparer
+      @factory.testing_comparer = testing_comparer
+      @ph_learn = @factory.build
+    end
+    it 'creates a learner with the learning comparer' do
+      erc_learner = @ph_learn.erc_learner
+      learn_loser_selector = erc_learner.loser_selector
+      # If a loser selector isn't externally provided to erc learner,
+      # the default isn't assigned until ErcLearning#run is called.
+      expect(learn_loser_selector).not_to be_nil
+      learn_comparer = learn_loser_selector.comparer
+      expect(learn_comparer).to eq learning_comparer
+    end
+    it 'creates a learner with the testing comparer' do
+      tester = @ph_learn.grammar_tester
+      test_loser_selector = tester.loser_selector
+      # If a loser selector isn't externally provided,
+      # the default isn't assigned until GrammarTest#run is called.
+      expect(test_loser_selector).not_to be_nil
+      test_comparer = test_loser_selector.comparer
+      expect(test_comparer).to eq testing_comparer
+    end
+  end
+
+  context 'without specifying a system' do
+    before(:example) do
+      @factory.learning_comparer = learning_comparer
+      @factory.testing_comparer = testing_comparer
+    end
+    it 'raises a RuntimeError' do
+      expect { @factory.build }.to raise_error(RuntimeError)
+    end
+  end
+  context 'without specifying a learning comparer' do
+    before(:example) do
+      @factory.system = system
+      @factory.testing_comparer = testing_comparer
+    end
+    it 'raises a RuntimeError' do
+      expect { @factory.build }.to raise_error(RuntimeError)
+    end
+  end
+  context 'without specifying a testing comparer' do
+    before(:example) do
+      @factory.system = system
+      @factory.learning_comparer = learning_comparer
+    end
+    it 'raises a RuntimeError' do
+      expect { @factory.build }.to raise_error(RuntimeError)
+    end
+  end
+end
