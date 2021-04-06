c1e99cdbd880d80702f6aa5e4bef2dbe7d654edd
Bruce Tesar <tesar@rutgers.edu>
2021-04-06 17:58:37 -0400

    Beefed up documentation for OTLearn::FactoryLearnTestMixin.

--- lib/otlearn/factory_learn_test_mixin.rb
+++ lib/otlearn/factory_learn_test_mixin.rb
@@ -18,9 +18,12 @@ module OTLearn
   # * _testing_comparer_ - returns the comparer object to be used
   #   during testing.
   module FactoryLearnTestMixin
-    # Checks that all required components are defined before
-    # building a learner.
-    # Returns true if no errors occurred.
+    # Checks that all required components (system, learning_comparer, and
+    # testing_comparer) are defined. Returns true if no errors occurred.
+    #
+    # Raises a RuntimeError if a component is missing.
+    # :call-seq:
+    #   check_factory_settings -> true
     def check_factory_settings
       # include the name of the host class in the error messages.
       msg1 = "#{self.class}#build:"
@@ -37,32 +40,48 @@ module OTLearn
 
       true # returns true if no errors were raised
     end
-    private :check_factory_settings
+    protected :check_factory_settings
 
+    # Returns a new OTLearn::ParadigmErcLearning object, set to use
+    # the provided _selector_ for loser selection.
+    # :call-seq:
+    #   create_para_erc_learner(selector) -> learner
     def create_para_erc_learner(selector)
       para_erc_learner = OTLearn::ParadigmErcLearning.new
       para_erc_learner.erc_learner = create_erc_learner(selector)
       para_erc_learner
     end
-    private :create_para_erc_learner
+    protected :create_para_erc_learner
 
+    # Returns a new OTLearn::LoserSelectorFromGen object, set to use
+    # the provided _comparer_.
+    # :call-seq:
+    #   create_loser_selector(comparer) -> selector
     def create_loser_selector(comparer)
       LoserSelectorFromGen.new(system, comparer)
     end
-    private :create_loser_selector
+    protected :create_loser_selector
 
+    # Returns a new OTLearn::ErcLearning object, set to use
+    # the provided _selector_ for loser selection.
+    # :call-seq:
+    #   create_erc_learner(selector) -> learner
     def create_erc_learner(selector)
       erc_learner = OTLearn::ErcLearning.new
       erc_learner.loser_selector = selector
       erc_learner
     end
-    private :create_erc_learner
+    protected :create_erc_learner
 
+    # Returns a new OTLearn::GrammarTest object, set to use
+    # the provided _selector_ for loser selection.
+    # :call-seq:
+    #   create_grammar_tester(selector) -> tester
     def create_grammar_tester(selector)
       tester = OTLearn::GrammarTest.new
       tester.loser_selector = selector
       tester
     end
-    private :create_grammar_tester
+    protected :create_grammar_tester
   end
 end
