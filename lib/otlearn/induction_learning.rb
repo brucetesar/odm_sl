59ad5774744b2843454a3997d762591c3394774b
Bruce Tesar <tesar@rutgers.edu>
2021-04-08 02:16:39 -0400

    Refactored OTLearn::InductionLearning, pulling out part of the #run method into a separate private method called by #run.

--- lib/otlearn/induction_learning.rb
+++ lib/otlearn/induction_learning.rb
@@ -6,7 +6,6 @@ require 'otlearn/consistency_checker'
 require 'otlearn/grammar_test'
 require 'otlearn/fewest_set_features'
 require 'otlearn/max_mismatch_ranking'
-require 'otlearn/otlearn'
 require 'otlearn/induction_step'
 
 module OTLearn
@@ -44,28 +43,42 @@ module OTLearn
     def run(output_list, grammar)
       # Test the winners to see which ones currently fail
       prior_result = grammar_tester.run(output_list, grammar)
-      failed_winners = prior_result.failed_winners.map(&:output)
-      # If there are no failed winners, raise an exception, because
-      # induction learning shouldn't be called unless there are failed
-      # winners to work on.
-      if failed_winners.empty?
-        raise 'InductionLearning invoked with no failed winners.'
-      end
-
-      # Collect those winners that are mismatch consistent with the grammar.
-      consistent_list = failed_winners.select do |output|
-        consistency_checker.mismatch_consistent?([output], grammar)
-      end
+      # find those failed winner outputs that are mismatch consistent
+      consistent_list = consistent_failed_winners(grammar, prior_result)
       # If there are no consistent failed winners, run FSF.
       # Otherwise, run MMR.
       substep = if consistent_list.empty?
-                  @fsf_learner.run(output_list, grammar, prior_result)
+                  fsf_learner.run(output_list, grammar, prior_result)
                 else
-                  @mmr_learner.run(consistent_list, grammar)
+                  mmr_learner.run(consistent_list, grammar)
                 end
       changed = substep.changed?
       test_result = grammar_tester.run(output_list, grammar)
       InductionStep.new(substep, test_result, changed)
     end
+
+    # Returns an array of all the outputs that both:
+    # * currently fail grammar testing (grammar's ranking doesn't uniquely
+    #   map the mismatched input to the correct output).
+    # * are nevertheless mismatch-consistent (the mismatched input
+    #   candidate is grammatical under some ranking consistent with the
+    #   learner's support).
+    # :call-seq:
+    #   consistent_failed_winners(grammar, prior_result) -> array
+    def consistent_failed_winners(grammar, prior_result)
+      failed_winners = prior_result.failed_winners.map(&:output)
+      # If there are no failed winners, raise an exception, because
+      # induction learning shouldn't be called unless there are failed
+      # winners to work on.
+      msg1 = 'InductionLearning invoked with no failed winners.'
+      raise msg1 if failed_winners.empty?
+
+      # Collect those winners that are mismatch consistent with the grammar,
+      # and return an array of them.
+      failed_winners.select do |output|
+        consistency_checker.mismatch_consistent?([output], grammar)
+      end
+    end
+    private :consistent_failed_winners
   end
 end
