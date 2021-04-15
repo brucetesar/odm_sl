b2dd9987d257cb1e99b6cfd90a5e04e3cafeae5d
Bruce Tesar <tesar@rutgers.edu>
2021-04-15 19:49:49 -0400

    Deleted some now-obsolete bin scripts, and an associated cucumber feature.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 # Environment information for Cucumber.
 
 # Add the project directory to the loadpath.
-# This allows features to be defined with purely project-relative paths,
-# e.g., "bin/sl/learn_l20_1r1s.rb".
+# This allows features to be defined with purely project-relative paths.
+# e.g., "bin/mysubdir/myfile.rb".
 project_dir = File.expand_path('../../..',__FILE__)
 $LOAD_PATH << File.expand_path(project_dir)
