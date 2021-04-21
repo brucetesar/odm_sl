f050c650c867795ee1740df0240b98e6b2926a78
Bruce Tesar <tesar@rutgers.edu>
2021-04-20 20:13:49 -0400

    Code and comment clean-up for class Sheet.

--- lib/sheet.rb
+++ lib/sheet.rb
@@ -23,9 +23,9 @@ class Sheet
     @image = [[nil]]
   end
 
-  # Returns a new sheet with dimensions the same as array +arr+, with
+  # Returns a new sheet with dimensions the same as array _arr_, with
   # the value of each cell of the sheet set to the value of the
-  # corresponding (by indices) element of +arr+.
+  # corresponding (by indices) element of _arr_.
   def self.new_from_a(arr)
     return Sheet.new if arr.nil?
 
@@ -46,43 +46,43 @@ class Sheet
     @image
   end
 
-  # Returns the value of the sheet cell at row +row+ and column +col+.
+  # Returns the value of the sheet cell at row _row_ and column _col_.
   def [](row, col)
     return nil if row > row_count || col > col_count
 
     @image[row - 1][col - 1]
   end
 
-  # Sets the value of the sheet cell at row +row+ and column +col+ to
+  # Sets the value of the sheet cell at row _row_ and column _col_ to
   # be +value+.
   def []=(row, col, value)
     extend_bounds(Cell.new(row, col), ONE_CELL_RANGE)
     @image[row - 1][col - 1] = value
   end
 
-  # Returns the value of the cell at the address stored in +cell+.
+  # Returns the value of the cell at the address stored in _cell_.
   def get_cell(cell)
     return nil if cell.row > row_count || cell.col > col_count
 
     @image[cell.row - 1][cell.col - 1]
   end
 
-  # Sets the value of the cell at the address stored in +cell+ to be +value+.
+  # Sets the value of the cell at the address stored in _cell_ to be
+  # _value_.
   def put_cell(cell, value)
     extend_bounds(Cell.new(cell.row, cell.col), ONE_CELL_RANGE)
     @image[cell.row - 1][cell.col - 1] = value
   end
 
-  # Returns a Sheet containing the content of the range +source_range+ of self.
+  # Returns a Sheet containing the content of the range _source_range_
+  # of self.
   # The first cell of the returned sheet contains the contents of
-  # the first cell of +source_range+ in self, and so forth.
+  # the first cell of _source_range_ in self, and so forth.
   # The returned sheet contains references to the same objects as
   # self's original image range.
-  # If +source_range+ goes beyond the bounds of self, the out-of-bounds cells
-  # in the returned sheet will contain nil.
+  # If _source_range_ goes beyond the bounds of self, the out-of-bounds
+  # cells in the returned sheet will contain nil.
   def get_range(source_range)
-    # Create a new Sheet, to be filled with the values of
-    # the source_range in self.
     target = Sheet.new
     # Iterate over the range in the source (self), putting the values
     # into the target.
@@ -95,14 +95,12 @@ class Sheet
     target
   end
 
-  # Replaces the values in +self+ of the range starting in +cell_first+ with
-  # the corresponding values in +source+.
+  # Replaces the values in _self_ of the range starting in _cell_first_
+  # with the corresponding values in _source_. Returns _source_.
   #
-  # If the size of +source+ is such that it would exceed the existing
-  # bounds of +self+, then +self+ is first expanded with additional rows
+  # If the size of _source_ is such that it would exceed the existing
+  # bounds of _self_, then _self_ is first expanded with additional rows
   # and/or columns to accommodate (new cells are initialized to nil).
-  #
-  # Returns +source+.
   def put_range_to_cell(cell_first, source)
     extend_bounds(cell_first, source)
     # Iterate over the source, putting the values into the range
@@ -116,24 +114,23 @@ class Sheet
     source
   end
 
-  # Replaces the values in +self+ of the range starting in the cell at
-  # position [+row+,+col+], replacing them with the corresponding values
-  # in +source+.
-  # Returns +source+.
+  # Replaces the values in _self_ of the range starting in the cell at
+  # position [_row_, _col_], replacing them with the corresponding values
+  # in _source_. Returns _source_.
   def put_range(row, col, source)
     cell_first = Cell.new(row, col)
     put_range_to_cell(cell_first, source)
   end
 
-  # Adds an empty row to the end of the sheet;
-  # empty meaning each entry is nil.
+  # Adds an empty row to the end of the sheet; empty meaning each entry
+  # is nil. Returns nil.
   def add_empty_row
     self[row_count + 1, 1] = nil
   end
 
-  # Adds the values in +source+ to the end of the sheet.
-  # If +start_col+ is specified, it adds the values starting in that column;
-  # by default, it starts in column 1.
+  # Adds the values in _source_ to the end of the sheet.
+  # If _start_col_ is specified, it adds the values starting in that
+  # column; by default, it starts in column 1.
   def append(source, start_col: 1)
     put_range(row_count + 1, start_col, source)
   end
@@ -158,15 +155,15 @@ class Sheet
   # *** Class Methods ***
   # *********************
 
-  # Returns the translation of +cell+ relative to +ref_cell+.
-  # If the original frame of reference for +cell+ (starting at row 1, col 1)
+  # Returns the translation of _cell_ relative to _ref_cell_.
+  # If the original frame of reference for _cell_ (starting at row 1, col 1)
   # was inserted into a new frame of reference so that the original started
-  # at +ref_cell+ in the new frame, then +cell+ of the original frame would
+  # at _ref_cell_ in the new frame, then _cell_ of the original frame would
   # correspond, in the new frame, to the cell returned by this method.
   #
-  # This is useful for translating cells when one sheet is embedded somewhere
-  # within another sheet; +ref_cell+ is the beginning of the range in the
-  # new sheet where the original sheet is being embedded.
+  # This is useful for translating cells when one sheet is embedded
+  # somewhere within another sheet; _ref_cell_ is the beginning of the
+  # range in the new sheet where the original sheet is being embedded.
   #
   # Sheet.translate_cell() is the reverse of Sheet.relative_to_cell().
   #---
@@ -179,21 +176,22 @@ class Sheet
              cell.col + ref_cell.col - 1)
   end
 
-  # Returns the address of +cell+ relative to +ref_cell+
-  # If, within the original frame of reference for +cell+ (starting at
+  # Returns the address of _cell_ relative to _ref_cell_.
+  # If, within the original frame of reference for _cell_ (starting at
   # row 1, col 1), a new frame of reference were extracted starting at
-  # +ref_cell+ of the original frame,  then +cell+ of the original frame
-  # would correspond, in the new frame, to the cell returned by this method.
+  # _ref_cell_ of the original frame,  then _cell_ of the original frame
+  # would correspond, in the new frame, to the cell returned by this
+  # method.
   #
   # This is useful when a range is being extracted from an original sheet
-  # to form its own sheet. +ref_cell+ is the first cell in the original
+  # to form its own sheet. _ref_cell_ is the first cell in the original
   # sheet where the range is being extracted from.
   #
   # Sheet.relative_to_cell() is the reverse of Sheet.translate_cell().
   #---
   # Because sheets start their indexing from 1, the location of a cell
-  # relative to a ref cell is accomplished with the formula (for both row and
-  # col) of
+  # relative to a ref cell is accomplished with the formula (for both row
+  # and col) of
   #   cell - ref_cell + 1
   # The location of cell (3,4) relative to cell (2,3) is (2,2),
   def self.relative_to_cell(cell, ref_cell)
@@ -206,19 +204,21 @@ class Sheet
   # ***********************
 
   # Extend the boundaries of the sheet just enough to accommodate
-  # the +range_image+ being inserted at the +start_cell+.
+  # the _range_image_ being inserted at the _start_cell_.
+  # Returns true if the sheet's boundaries were extended at all, and
+  # false otherwise.
   def extend_bounds(start_cell, range_image)
     row_last = start_cell.row + range_image.row_count - 1
     col_last = start_cell.col + range_image.col_count - 1
     extra_rows = row_last - row_count
-    add_rows(extra_rows) if extra_rows > 0
+    add_rows(extra_rows) if extra_rows.positive?
     extra_cols = col_last - col_count
-    add_cols(extra_cols) if extra_cols > 0
-    (extra_rows > 0) || (extra_cols > 0)
+    add_cols(extra_cols) if extra_cols.positive?
+    extra_rows.positive? || extra_cols.positive?
   end
   private :extend_bounds
 
-  # Add +num+ rows to the end of the sheet.
+  # Add _num_ rows to the end of the sheet.
   def add_rows(num)
     num.times do
       new_row = []
@@ -229,7 +229,7 @@ class Sheet
   end
   private :add_rows
 
-  # Add +num+ columns to the right edge of the sheet.
+  # Add _num_ columns to the right edge of the sheet.
   def add_cols(num)
     @image.each do |row|
       num.times { row << nil }
