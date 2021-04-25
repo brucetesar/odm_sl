6b4967001df78b41967d88ba3862cb8e685f05de
Bruce Tesar <tesar@rutgers.edu>
2021-04-24 21:28:53 -0400

    Rewrote the equality methods for Output, and slightly simplified Output#dup. Enhanced some documentation.

--- lib/output.rb
+++ lib/output.rb
@@ -5,30 +5,37 @@
 # An Output is a list of correspondence elements, with an associated
 # morphological word.
 class Output < Array
-  # the morphword associated with the output.
+  # The morphword associated with the output.
   attr_accessor :morphword
 
-  # A newly created output is empty, with no morphological word, so that
-  # it can be built up piece by piece.
+  # Returns a new output object, empty and with no morphological word.
+  # :call-seq:
+  #   new() -> output
   def initialize
     @morphword = nil
   end
 
-  # Returns a copy of the output, containing a duplicate of each
+  # Returns a duplicate of the output, containing a duplicate of each
   # correspondence element and a duplicate of the morphological word.
+  # :call-seq:
+  #   dup() -> output
   def dup
     # Call Array#map to get an array of dups of the elements, and add
     # them to a new Output.
     copy = Output.new.concat(super.map(&:dup))
-    copy.morphword = @morphword.dup unless @morphword.nil?
+    copy.morphword = @morphword.dup
     copy
   end
 
   # Two outputs are the same if they contain equivalent elements.
   # The morphological words are *not* checked for equality.
   # Equivalent to eql?().
+  # :call-seq:
+  #   output == obj -> boolean
   def ==(other)
-    return false unless super
+    return false unless size == other.size
+
+    each_index { |idx| return false unless self[idx] == other[idx] }
 
     true
   end
@@ -36,16 +43,23 @@ class Output < Array
   # Two outputs are the same if they contain equivalent elements.
   # The morphological words are *not* checked for equality.
   # Equivalent to ==().
+  # :call-seq:
+  #   eql?(other) -> boolean
   def eql?(other)
     self == other
   end
 
-  # Returns a string containing the to_s() of each element in the output.
+  # Returns a string containing the to_s() of each element in the output,
+  # concatenated in order with no separators.
+  # :call-seq:
+  #   to_s() -> string
   def to_s
     join
   end
 
-  # Determines whether there is main stress in the output.
+  # Returns true if one of the syllables has main stress, false otherwise.
+  # :call-seq:
+  #   main_stress?() -> boolean
   def main_stress?
     any?(&:main_stress?)
   end
