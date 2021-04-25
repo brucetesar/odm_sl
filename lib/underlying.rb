769fbddcd023a52880e16018f99e4814ce697832
Bruce Tesar <tesar@rutgers.edu>
2021-04-25 15:24:07 -0400

    Created specs for Underlying, and did some code and documentation clean-up on Underlying.

--- lib/underlying.rb
+++ lib/underlying.rb
@@ -1,25 +1,32 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
-#
 
 # An underlying form is the phonological representation associated with
 # a morpheme, in the form of a list of correpondence elements.
+#
+# Output subclasses from Array, and so inherits a variety of methods.
 class Underlying < Array
-
   # Returns an empty Underlying object.
-  def initialize
-  end
+  # :call-seq:
+  #   new() -> underlying
+  def initialize; end
 
-  # Returns a copy of the underlying form, containing a copy of
-  # each of the correspondence elements of the original.
+  # Returns a deep copy duplicate of the underlying form, containing
+  # a duplicate of each of the correspondence elements of the original.
+  # :call-seq:
+  #   dup() -> underlying
   def dup
     copy = Underlying.new
-    self.each{|el| copy << el.dup}
-    return copy
+    each { |el| copy << el.dup }
+    copy
   end
 
   # String form is the concatenation of the string form of each
   # element in the underlying form (with no separating symbols).
+  # :call-seq:
+  #   to_s() -> string
   def to_s
-    self.join
+    join
   end
-end # class Underlying
+end
