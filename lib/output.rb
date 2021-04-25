930d43ad741c9ad569282b7172a5869b0d9d9a61
Bruce Tesar <tesar@rutgers.edu>
2021-04-24 21:11:05 -0400

    Greatly expanded the specs for class Output. Also did some code cleanup on Output.

--- lib/output.rb
+++ lib/output.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 # Author: Bruce Tesar
-#
 
 # An Output is a list of correspondence elements, with an associated
 # morphological word.
 class Output < Array
+  # the morphword associated with the output.
+  attr_accessor :morphword
 
   # A newly created output is empty, with no morphological word, so that
   # it can be built up piece by piece.
@@ -11,48 +14,39 @@ class Output < Array
     @morphword = nil
   end
 
-  # Returns a reference to the output's morphological word.
-  def morphword
-    @morphword
-  end
-
-  # Sets the output's morphological word to the parameter _mw_.
-  def morphword=(mw)
-    @morphword = mw
-  end
-
   # Returns a copy of the output, containing a duplicate of each
   # correspondence element and a duplicate of the morphological word.
   def dup
     # Call Array#map to get an array of dups of the elements, and add
     # them to a new Output.
-    copy = Output.new.concat(super.map { |el| el.dup })
+    copy = Output.new.concat(super.map(&:dup))
     copy.morphword = @morphword.dup unless @morphword.nil?
-    return copy
+    copy
   end
- 
+
   # Two outputs are the same if they contain equivalent elements.
   # The morphological words are *not* checked for equality.
+  # Equivalent to eql?().
   def ==(other)
     return false unless super
+
     true
   end
 
+  # Two outputs are the same if they contain equivalent elements.
+  # The morphological words are *not* checked for equality.
   # Equivalent to ==().
   def eql?(other)
-    self==other
+    self == other
   end
 
-  # Returns a string containing the #to_s() of each element in the output.
+  # Returns a string containing the to_s() of each element in the output.
   def to_s
-    self.join
+    join
   end
-  
+
   # Determines whether there is main stress in the output.
   def main_stress?
-    any? do |syl|
-      syl.main_stress?
-    end
+    any?(&:main_stress?)
   end
-  
-end # class Output
+end
