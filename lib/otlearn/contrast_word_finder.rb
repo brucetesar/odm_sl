86c8af21a42e4ad6f4920819c3a1dc1f91ab650b
Bruce Tesar <tesar@rutgers.edu>
2021-05-02 03:08:54 -0400

    Refactored OTLearn::ContrastWordFinder, enhanced it so that words for which neither of the contrasting morphemes has an unset feature will be returned.

--- lib/otlearn/contrast_word_finder.rb
+++ lib/otlearn/contrast_word_finder.rb
@@ -23,12 +23,14 @@ module OTLearn
     # :call-seq:
     #   new(grammar) -> finder
     #--
-    # Named parameters contrast_matcher and alt_env_finder are dependency
-    # injections used in testing.
-    def initialize(grammar, contrast_matcher: nil, alt_env_finder: nil)
+    # Named parameters contrast_matcher, alt_env_finder and word_search
+    # are dependency injections used in testing.
+    def initialize(grammar, contrast_matcher: nil, alt_env_finder: nil,
+                   word_search: nil)
       @grammar = grammar
       @cw_matcher = contrast_matcher
       @alt_env_finder = alt_env_finder || AltEnvFinder.new(@grammar)
+      @word_search = word_search || WordSearch.new
     end
 
     # Returns an array of words in _others_ that form valid contrast pairs
@@ -39,18 +41,41 @@ module OTLearn
       ref_mw = ref_word.morphword
       cword_list = []
       ref_mw.each do |m|
-        # find words that morphologically match in all but the target morpheme
-        # TODO: have cw_matcher return the morpheme constrasting with m, or
-        # nil if there is no match. Then, check if at least one of m and
-        # its contrasting morpheme have an unset feature. If not, don't add
-        # the word to mw_matches (nothing to set).
-        mw_matches =
-          others.find_all { |o| @cw_matcher.match?(ref_mw, m, o.morphword) }
+        # find each word that morphologically matches in all but the target
+        # morpheme, paired with the contrasting morpheme of the word.
+        mw_matches = find_morphword_matches(ref_mw, m, others)
+        # Only keep words where one of the contrasting morphemes has an
+        # unset feature.
+        match_words = check_for_unset_features(m, mw_matches)
         # find words alternating in an unset feature relative to ref_word
-        alternating_words = @alt_env_finder.find(ref_word, m, mw_matches)
+        alternating_words = @alt_env_finder.find(ref_word, m, match_words)
         alternating_words.each { |w| cword_list << w }
       end
       cword_list
     end
+
+    def find_morphword_matches(ref_mw, morph, others)
+      others.each_with_object([]) do |o, memo|
+        match_morph = @cw_matcher.match(ref_mw, morph, o.morphword)
+        memo << [match_morph, o] unless match_morph.nil?
+      end
+    end
+    private :find_morphword_matches
+
+    def check_for_unset_features(morph, mw_matches)
+      # if the target morpheme has an unset feature, return all of the
+      # mw match words.
+      return mw_matches.map { |pair| pair[1] }\
+        unless @word_search.find_unset_features([morph], @grammar).empty?
+
+      # Target morph has no unset features, so check each contrast morph
+      # for unset features, returning only those words with unset
+      # contrast morpheme features.
+      unset_matches = mw_matches.reject do |pair|
+        @word_search.find_unset_features([pair[0]], @grammar).empty?
+      end
+      unset_matches.map { |pair| pair[1] }
+    end
+    private :check_for_unset_features
   end
 end
--- spec/otlearn/contrast_word_finder_spec.rb
+++ spec/otlearn/contrast_word_finder_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe 'OTLearn::ContrastWordFinder' do
   let(:grammar) { double('grammar') }
   let(:matcher) { double('contrast matcher') }
   let(:alt_env_finder) { double('alt_env_finder') }
+  let(:word_search) { double('word_search') }
   let(:m1) { double('m1') }
   let(:m2) { double('m2') }
   let(:m3) { double('m3') }
@@ -16,7 +17,8 @@ RSpec.describe 'OTLearn::ContrastWordFinder' do
   before(:example) do
     @finder = OTLearn::ContrastWordFinder.new(grammar,
                                               contrast_matcher: matcher,
-                                              alt_env_finder: alt_env_finder)
+                                              alt_env_finder: alt_env_finder,
+                                              word_search: word_search)
   end
 
   context 'given several words' do
@@ -29,25 +31,31 @@ RSpec.describe 'OTLearn::ContrastWordFinder' do
       allow(m1m4).to receive(:morphword).and_return([m1, m4])
       allow(m3m2).to receive(:morphword).and_return([m3, m2])
       allow(m3m4).to receive(:morphword).and_return([m3, m4])
-      allow(matcher).to receive(:match?).with([m1, m2], m1, [m1, m4])\
-                                        .and_return(false)
-      allow(matcher).to receive(:match?).with([m1, m2], m1, [m3, m2])\
-                                        .and_return(true)
-      allow(matcher).to receive(:match?).with([m1, m2], m1, [m3, m4])\
-                                        .and_return(false)
-      allow(matcher).to receive(:match?).with([m1, m2], m2, [m1, m4])\
-                                        .and_return(true)
-      allow(matcher).to receive(:match?).with([m1, m2], m2, [m3, m2])\
-                                        .and_return(false)
-      allow(matcher).to receive(:match?).with([m1, m2], m2, [m3, m4])\
-                                        .and_return(false)
+      allow(matcher).to receive(:match).with([m1, m2], m1, [m1, m4])\
+                                       .and_return(nil)
+      allow(matcher).to receive(:match).with([m1, m2], m1, [m3, m2])\
+                                       .and_return(m3)
+      allow(matcher).to receive(:match).with([m1, m2], m1, [m3, m4])\
+                                       .and_return(nil)
+      allow(matcher).to receive(:match).with([m1, m2], m2, [m1, m4])\
+                                       .and_return(m4)
+      allow(matcher).to receive(:match).with([m1, m2], m2, [m3, m2])\
+                                       .and_return(nil)
+      allow(matcher).to receive(:match).with([m1, m2], m2, [m3, m4])\
+                                       .and_return(nil)
       @others = [m1m4, m3m2, m3m4]
     end
     context 'with unset alternating environment morphemes' do
       before(:example) do
+        # m2 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m2], grammar).and_return([:found_feature])
         # m2 has an unset feature that alternates for m1m2, m3m2
         allow(alt_env_finder).to receive(:find).with(m1m2, m1, [m3m2])\
                                                .and_return([m3m2])
+        # m1 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m1], grammar).and_return([:found_feature])
         # m1 has an unset feature that alternates for m1m2, m1m4
         allow(alt_env_finder).to receive(:find).with(m1m2, m2, [m1m4])\
                                                .and_return([m1m4])
@@ -59,17 +67,76 @@ RSpec.describe 'OTLearn::ContrastWordFinder' do
     end
     context 'with one unset alternating environment morpheme' do
       before(:example) do
+        # m2 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m2], grammar).and_return([:found_feature])
         # m2 has an unset feature that alternates for m1m2, m3m2
         allow(alt_env_finder).to receive(:find).with(m1m2, m1, [m3m2])\
                                                .and_return([m3m2])
-        # m1 either has no unset feature, or the unset feature doesn't
-        # alternate for m1m2, m1m4
+        # m1 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m1], grammar).and_return([:found_feature])
+        # m1 has no unset feature that alternates for m1m2, m1m4
         allow(alt_env_finder).to receive(:find).with(m1m2, m2, [m1m4])\
                                                .and_return([])
         @contrast_words = @finder.contrast_words(m1m2, @others)
       end
-      it 'returns only the word with the unset alt env morpheme' do
+      it 'returns the word with the unset contrast morpheme' do
         expect(@contrast_words).to contain_exactly(m3m2)
+      end
+    end
+    context 'with no unset target morph features, one unset contrast morph' do
+      before(:example) do
+        # m2 has no unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m2], grammar).and_return([])
+        # m2 has no unset feature that alternates for m1m2, m3m2
+        allow(alt_env_finder).to receive(:find).with(m1m2, m1, [m3m2])\
+                                               .and_return([])
+        # m1 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m1], grammar).and_return([:found_feature])
+        # m1 has an unset feature that alternates for m1m2, m1m4
+        allow(alt_env_finder).to receive(:find).with(m1m2, m2, [m1m4])\
+                                               .and_return([m1m4])
+        # m4 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m4], grammar).and_return([:found_feature])
+        @contrast_words = @finder.contrast_words(m1m2, @others)
+      end
+      it 'returns the word with the unset contrast morpheme' do
+        expect(@contrast_words).to contain_exactly(m1m4)
+      end
+    end
+    context 'with no unset target morph, no unset contrast morph' do
+      before(:example) do
+        # m2 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m2], grammar).and_return([:found_feature])
+        # m2 has an unset feature, but m3m2 is ineligible
+        allow(alt_env_finder).to receive(:find).with(m1m2, m1, [])\
+                                               .and_return([])
+        # m1 has no unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m1], grammar).and_return([])
+        # m1 has no unset feature that alternates for m1m2, m1m4
+        allow(alt_env_finder).to receive(:find).with(m1m2, m2, [m1m4])\
+                                               .and_return([])
+        # m3 has no unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m3], grammar).and_return([])
+        # m4 has an unset feature
+        allow(word_search).to receive(:find_unset_features)\
+          .with([m4], grammar).and_return([:found_feature])
+        @contrast_words = @finder.contrast_words(m1m2, @others)
+      end
+      # m3m2 blocked b/c neither m1 nor m3 has an unset feature.
+      # m1m4 blocked b/c m1 has no unset, thus no unset alternating feature.
+      it 'returns no words' do
+        expect(@contrast_words).to be_empty
+      end
+      it 'checked for unset environment features for m1m4' do
+        expect(alt_env_finder).to have_received(:find).with(m1m2, m2, [m1m4])
       end
     end
   end
