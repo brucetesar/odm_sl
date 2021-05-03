6f60560848e1fd811b202e5afaec5f6367b5b590
Bruce Tesar <tesar@rutgers.edu>
2021-05-02 20:34:38 -0400

    Implementations and specs for OTLearn::ContrastWordMatcher.

---
+++ lib/otlearn/contrast_word_matcher.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+module OTLearn
+  # Matches two words, a reference word and an other word,
+  # morpheme-by-morpheme in linear order. If they different in the
+  # target morpheme, but have the same morpheme in every other position,
+  # then the words properly match. See documentation for the method #match().
+  class ContrastWordMatcher
+    # Returns a new contrast word matcher object.
+    # :call-seq:
+    #   new() -> matcher
+    def initialize; end
+
+    # Tries to match the morphwords _ref_mw_ and _other_mw_. The other
+    # morphwword qualifies as a contrast word with respect to _morph_
+    # if all of the following criteria are met:
+    # * _ref_mw_ and _other_mw_ have the same number of morphemes.
+    # * the target morpheme _morph_ is a morpheme of _ref_mw_.
+    # * the morpheme in _other_mw_ corresponding to _morph_ in _ref_mw_
+    #   is not the same as _morph_ (contrast for target morpheme).
+    # * every other morpheme of _ref_mw_ is the same as its corresponding
+    #   morpheme in _other_mw_ (identity of the environment morphemes).
+    # If _other_mw_ qualifies as a contrast word, then the morpheme
+    # of _other_mw_ corresponding to _morph_ is returned. Otherwise,
+    # nil is returned.
+    # Raises a RuntimeError if the target morpheme _morph_ occurs more
+    # than once in _ref_mw_.
+    # :call-seq:
+    #   match(ref_mw, morph, other_mw) -> morpheme or nil
+    def match(ref_mw, morph, other_mw)
+      return nil unless edge_conditions_met?(ref_mw, morph, other_mw)
+
+      contrast_morph = nil
+      # iterate through the words morpheme by morpheme
+      ref_mw.each_index do |idx|
+        # boolean: are the corresponding morphemes not the same?
+        morph_mismatch = ref_mw[idx] != other_mw[idx]
+        # boolean: is the current ref word morpheme also the target?
+        target_match = ref_mw[idx] == morph
+        contrast_morph = other_mw[idx] if target_match && morph_mismatch
+        return nil if !target_match && morph_mismatch
+      end
+      contrast_morph
+    end
+
+    # Checks edge conditions. Returns true if they are all met. Otherwise,
+    # either returns nil or raises a RuntimeError.
+    def edge_conditions_met?(ref_mw, morph, other_mw)
+      # words with differing numbers of morphemes cannot match
+      return nil if ref_mw.size != other_mw.size
+
+      # Cannot have the target morph appear multiple times
+      msg = 'ContrastWordMatcher#match: target word cannot appear multiple times'
+      raise msg if ref_mw.find_all { |m| m == morph }.size > 1
+
+      true
+    end
+    private :edge_conditions_met?
+  end
+end
---
+++ spec/otlearn/contrast_word_matcher_spec.rb
@@ -0,0 +1,95 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+require 'rspec'
+require 'otlearn/contrast_word_matcher'
+
+RSpec.describe 'OTLearn::ContrastWordMatcher' do
+  let(:m1) { double('morph1') }
+  let(:m2) { double('morph2') }
+  let(:m3) { double('morph3') }
+  let(:m4) { double('morph4') }
+  before(:example) do
+    @matcher = OTLearn::ContrastWordMatcher.new
+  end
+
+  context 'when target_mw and other_mw have different sizes' do
+    before(:example) do
+      ref_mw = [m1, m2, m3]
+      other_mw = [m1, m4]
+      @target_morph = m2
+      @contrast_morph = @matcher.match(ref_mw, @target_morph, other_mw)
+    end
+    it 'returns nil' do
+      expect(@contrast_morph).to be_nil
+    end
+  end
+  context 'when other differs only in the target morpheme' do
+    before(:example) do
+      ref_mw = [m1, m2, m3]
+      other_mw = [m1, m4, m3]
+      @target_morph = m2
+      @contrast_morph = @matcher.match(ref_mw, @target_morph, other_mw)
+    end
+    it 'returns the contrast morpheme' do
+      expect(@contrast_morph).to eq m4
+    end
+  end
+  context 'when other does not differ in the target morpheme' do
+    before(:example) do
+      ref_mw = [m1, m2, m3]
+      other_mw = [m1, m2, m4]
+      @target_morph = m2
+      @contrast_morph = @matcher.match(ref_mw, @target_morph, other_mw)
+    end
+    it 'returns nil' do
+      expect(@contrast_morph).to be_nil
+    end
+  end
+  context 'when other differs in the target morpheme and another' do
+    before(:example) do
+      ref_mw = [m2, m1]
+      other_mw = [m4, m3]
+      @target_morph = m2
+      @contrast_morph = @matcher.match(ref_mw, @target_morph, other_mw)
+    end
+    it 'returns nil' do
+      expect(@contrast_morph).to be_nil
+    end
+  end
+  context 'when reference and other are contrasting monomorphemic words' do
+    before(:example) do
+      ref_mw = [m3]
+      other_mw = [m4]
+      @target_morph = m3
+      @contrast_morph = @matcher.match(ref_mw, @target_morph, other_mw)
+    end
+    it 'returns the contrasting morpheme' do
+      expect(@contrast_morph).to eq m4
+    end
+  end
+  context 'when reference does not contain the target morpheme' do
+    before(:example) do
+      ref_mw = [m1, m3]
+      other_mw = [m1, m4]
+      @target_morph = m4
+      @contrast_morph = @matcher.match(ref_mw, @target_morph, other_mw)
+    end
+    it 'returns nil' do
+      expect(@contrast_morph).to be_nil
+    end
+  end
+  context 'when reference contains multiple occurrences of the target morpheme' do
+    before(:example) do
+      @ref_mw = [m1, m3, m1]
+      @other_mw = [m4, m3, m2]
+      @target_morph = m1
+    end
+    it 'raises a RuntimeError' do
+      msg = 'ContrastWordMatcher#match: target word cannot appear multiple times'
+      expect { @matcher.match(@ref_mw, @target_morph, @other_mw) }.to\
+        raise_error(RuntimeError, msg)
+    end
+  end
+end
