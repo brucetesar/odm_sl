46a8600ec20ac2f72523bd4b03a658b75c81edac
Bruce Tesar <tesar@rutgers.edu>
2021-05-20 22:12:35 -0400

    Changed the acceptance specs in sl_learning_spec.rb to simply check if the learning result was successful, instead of comparing an entire output file to a fixture.

--- spec/acceptance/sl_learning_spec.rb
+++ spec/acceptance/sl_learning_spec.rb
@@ -3,47 +3,30 @@
 # Author: Bruce Tesar
 #
 # This acceptance spec runs learning on all 24 SL languages,
-# and checks the generated learning outputs against the test fixtures.
+# and verifies that learning was successful for all languages.
 
 require_relative '../../lib/odl/resolver'
-
 require 'otlearn/language_learning_factory'
 require 'otlearn/language_learning_runner'
 require 'sl/system'
 
-RSpec.describe 'Running ODL on SL', :acceptance do
+RSpec.describe OTLearn::LanguageLearningRunner, :acceptance do
+  data_file = File.join(ODL::DATA_DIR, 'sl', 'outputs_typology_1r1s.mar')
+
   before(:context) do
-    # Set up directory paths
-    data_dir = File.join(ODL::DATA_DIR, 'sl')
-    data_file = File.join(data_dir, 'outputs_typology_1r1s.mar')
-    @expected_dir =
-      File.join(ODL::PROJECT_DIR, 'test', 'fixtures', 'sl_learning')
-    @generated_dir = File.join(ODL::TEMP_DIR, 'sl_learning')
     # Configure and build the learner
     factory = OTLearn::LanguageLearningFactory.new
     factory.para_mark_low.learn_consistent.test_consistent
     factory.system = SL::System.instance
     learner = factory.build
-    # Run learning on all of the languages
-    runner = OTLearn::LanguageLearningRunner.new(factory.system, learner)
-    runner.prep_output_dir(@generated_dir, '*.csv')
-    runner.run_languages(data_file) do |label, outputs|
-      result = runner.run(label, outputs)
-      runner.write(result, out_dir: @generated_dir)
-    end
+    # Create the learning runner
+    @runner = described_class.new(factory.system, learner)
   end
 
-  (1..24).each do |num|
-    context "on language L#{num}" do
-      before do
-        # Read each file's contents into a string
-        @generated = IO.read "#{@generated_dir}/L#{num}.csv"
-        @expected = IO.read "#{@expected_dir}/L#{num}.csv"
-      end
-
-      it 'produces output that matches its test fixture' do
-        expect(@generated).to eq @expected
-      end
+  OTLearn::LanguageLearningRunner.run_languages(data_file) do |label, outputs|
+    it "sucessfully learns #{label}" do
+      @result = @runner.run(label, outputs)
+      expect(@result).to be_learning_successful
     end
   end
 end
