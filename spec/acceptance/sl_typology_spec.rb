ea3e7786a573b915d60aa45fd76cfca3e79d86d2
Bruce Tesar <tesar@rutgers.edu>
2021-05-21 22:49:29 -0400

    Switched the typology acceptance specs so that the typology is generated but not written to file, and instead compared directly with the data read in from the fixture file.

--- spec/acceptance/sl_typology_spec.rb
+++ spec/acceptance/sl_typology_spec.rb
@@ -1,41 +1,49 @@
 # frozen_string_literal: true
 
 # Author: Bruce Tesar
-#
+
 # This is an acceptance test for the typology generation for SL.
-# It tests the output created by bin/generate_typology_1r1s.rb.
 
 require_relative '../../lib/odl/resolver'
 require 'otlearn/language_learning_runner'
+require 'sl/system'
+require 'factorial_typology'
+require 'sync_enum'
 
-RSpec.describe 'Generating the 1r1s typology for SL', :acceptance do
-  before(:context) do
+RSpec.describe 'FactorialTypology', :acceptance do
+  context 'when generating the typology for SL 1r1s' do
+    # Generate the typology data
+    system = SL::System.instance
+    competition_list = system.generate_competitions_1r1s
+    ft_result = FactorialTypology.new(competition_list)
+    lang_list = ft_result.factorial_typology
+    generated_data = lang_list.map do |lang|
+      outputs = OTLearn::LanguageLearningRunner.wlp_to_learning_data(lang)
+      [lang.label, outputs]
+    end
+    # Retrieve the fixture data
     project_dir = ODL::PROJECT_DIR
-    @sl_fixture_dir = File.join(project_dir, 'test', 'fixtures', 'sl')
-    @generated_dir = File.join(project_dir, 'data', 'sl')
-    Dir.mkdir(@generated_dir) unless Dir.exist?(@generated_dir)
-    # The executable dir must be a relative path, otherwise a bunch of
-    # warnings are generated about redefining constants when the load
-    # command is executed. Maybe the paths fed to the require statements
-    # look different with an absolute path, and require fails to recognize
-    # that a file has already been required?
-    executable_dir = File.join('bin', 'sl')
-    load "#{executable_dir}/generate_typology_1r1s.rb"
-  end
-
-  before do
+    sl_fixture_dir = File.join(project_dir, 'test', 'fixtures', 'sl')
     fname = 'outputs_typology_1r1s.mar'
-    @fixture_file = File.join(@sl_fixture_dir, fname)
-    @generated_file = File.join(@generated_dir, fname)
-    @fixture_value = []
-    OTLearn::LanguageLearningRunner.read_languages(@fixture_file) \
-        { |label, outputs| @fixture_value << [label, outputs] }
-    @generated_value = []
-    OTLearn::LanguageLearningRunner.read_languages(@generated_file) \
-        { |label, outputs| @generated_value << [label, outputs] }
-  end
+    fixture_file = File.join(sl_fixture_dir, fname)
+    fixture_data = []
+    OTLearn::LanguageLearningRunner.read_languages(fixture_file) \
+      { |label, outputs| fixture_data << [label, outputs] }
+    iter = SyncEnum.new(generated_data.to_enum, fixture_data.to_enum)
 
-  it 'produces output that matches its test fixture' do
-    expect(@generated_value).to eq @fixture_value
+    it 'generates the correct number of languages for SL 1r1s' do
+      expect(generated_data.size).to eq fixture_data.size
+    end
+
+    loop do
+      gen_lang, fix_lang = iter.next
+      it "generates a language with the label #{fix_lang[0]}" do
+        expect(gen_lang[0]).to eq fix_lang[0]
+      end
+
+      it "generates the outputs for language #{fix_lang[0]}" do
+        expect(gen_lang[1]).to eq fix_lang[1]
+      end
+    end
   end
 end
