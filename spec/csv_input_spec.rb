38ea2c10aabb40f90669cb9fb2c93ab5529b6deb
Bruce Tesar <tesar@rutgers.edu>
2021-05-22 01:46:23 -0400

    Fixed style infractions in csv_input_spec.rb.

--- spec/csv_input_spec.rb
+++ spec/csv_input_spec.rb
@@ -5,33 +5,36 @@
 require_relative '../lib/odl/resolver'
 require 'csv_input'
 
-fixture_dir = File.join(ODL::SPEC_DIR, 'fixtures')
-
 RSpec.describe 'CsvInput' do
-  context 'when created with a valid CSV filename' do
-    before(:example) do
+  fixture_dir = File.join(ODL::SPEC_DIR, 'fixtures')
+
+  context 'when created with a valid CSV file' do
+    before do
       infile = File.join(fixture_dir, 'erc_input1.csv')
       @csv_input = CsvInput.new(infile)
     end
+
     it 'returns an array of column headers' do
-      expect(@csv_input.headers).to eq(['ERC_Label', 'Con1', 'Con2', 'Con3'])
+      expect(@csv_input.headers).to eq(%w[ERC_Label Con1 Con2 Con3])
     end
+
     it 'returns an array of arrays of the data' do
-      expect(@csv_input.data).to eq([['E1', 'W', 'L', 'W'],
-                                     ['E2', 'e', 'W', 'L']])
+      expect(@csv_input.data).to eq([%w[E1 W L W], %w[E2 e W L]])
     end
   end
 
   context 'when created with a file containing numeric data' do
-    before(:example) do
+    before do
       infile = File.join(fixture_dir, 'cand_input1.csv')
       @csv_input = CsvInput.new(infile)
     end
+
     it 'returns a data array of all strings' do
       expect(@csv_input.data[0]).to all be_a_kind_of(String)
     end
+
     it 'returns an array of arrays of the data' do
-      expect(@csv_input.data).to eq([['in1', 'out11', '0', '3', '2']])
+      expect(@csv_input.data).to eq([%w[in1 out11 0 3 2]])
     end
   end
 end
