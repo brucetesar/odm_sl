1ac39a34cfb9115ca2533fdb840f2edc645a38a8
Bruce Tesar <tesar@rutgers.edu>
2021-05-21 23:03:10 -0400

    Removed unnecessary definition of project directory.

--- spec/acceptance/sl_typology_spec.rb
+++ spec/acceptance/sl_typology_spec.rb
@@ -4,7 +4,6 @@
 
 # This is an acceptance test for the typology generation for SL.
 
-require_relative '../../lib/odl/resolver'
 require 'otlearn/language_learning_runner'
 require 'sl/system'
 require 'factorial_typology'
@@ -22,19 +21,17 @@ RSpec.describe 'FactorialTypology', :acceptance do
       [lang.label, outputs]
     end
     # Retrieve the fixture data
-    project_dir = ODL::PROJECT_DIR
-    sl_fixture_dir = File.join(project_dir, 'spec', 'fixtures', 'sl')
-    fname = 'outputs_typology_1r1s.mar'
-    fixture_file = File.join(sl_fixture_dir, fname)
+    fixture_file = File.join('spec', 'fixtures', 'sl',
+                             'outputs_typology_1r1s.mar')
     fixture_data = []
     OTLearn::LanguageLearningRunner.read_languages(fixture_file) \
       { |label, outputs| fixture_data << [label, outputs] }
-    iter = SyncEnum.new(generated_data.to_enum, fixture_data.to_enum)
 
     it 'generates the correct number of languages for SL 1r1s' do
       expect(generated_data.size).to eq fixture_data.size
     end
 
+    iter = SyncEnum.new(generated_data.to_enum, fixture_data.to_enum)
     loop do
       gen_lang, fix_lang = iter.next
       it "generates a language with the label #{fix_lang[0]}" do
