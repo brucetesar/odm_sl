b1e67dbeceb046c8f211cf5b842ad4e93b1cc825
Bruce Tesar <tesar@rutgers.edu>
2021-05-30 05:56:07 -0400

    Specs and implementation for OTLearn::InductiveFeatureValueFinder.

---
+++ lib/otlearn/inductive_feature_value_finder.rb
@@ -0,0 +1,131 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+require 'otlearn/consistency_checker'
+require 'word_search'
+require 'feature_value_pair'
+
+module OTLearn
+  # An object of this class, when method #run is called, takes a winner
+  # that is mismatch-inconsistent (the max-mismatch input for the word
+  # cannot be mapped to the output by any ranking consistent with the
+  # grammar), and searches for any unset features that, if set, would
+  # make the word mismatch-consistent. This class works in support of
+  # inductive learning, in particular in support of Fewest Set Features.
+  class InductiveFeatureValueFinder
+    # Determines if a set of outputs are consistent with a grammar.
+    attr_accessor :consistency_checker
+
+    # Search object containing #find_unset_features_in_words
+    attr_accessor :word_search
+
+    # Returns a new inductive feature value finder.
+    # :call-seq:
+    #   new -> finder
+    #--
+    # Named parameter _fv_pair_class_ is a dependency injection used for
+    # testing.
+    def initialize(fv_pair_class: nil)
+      @consistency_checker = nil
+      @word_search = nil
+      @fv_pair_class = fv_pair_class
+    end
+
+    # If any of the components has not been externally assigned, then
+    # assign its default value.
+    def check_defaults
+      @consistency_checker ||= ConsistencyChecker.new
+      @word_search ||= WordSearch.new
+      @fv_pair_class ||= FeatureValuePair
+      nil
+    end
+    private :check_defaults
+
+    # Packages together a failed winner with a list of feature-value pairs
+    # that, if set, would ensure that the winner was possibly optimal.
+    class WinnerValuePackage
+      # The winner rendered mismatch-consistent by setting feature value
+      # pairs.
+      attr_reader :winner
+
+      # The list of feature-value pairs rendering the winner
+      # mismatch-consistent.
+      attr_reader :values
+
+      # Returns a new winner/value package object.
+      # :call-seq:
+      #   new(winner, values) -> package
+      def initialize(winner, values)
+        @winner = winner
+        @values = values
+      end
+    end
+
+    # Returns an array of packages, each containing the winner and a
+    # minimal set of successful feature values.
+    def run(winner, grammar, test_result)
+      check_defaults
+      # Duplicate the winner, so that the input can be altered.
+      winner_dup = grammar.parse_output(winner.output)
+      # Find all unset features, then find which ones are successful
+      # (render the winner mismatch-consistent).
+      unset_uf_features =
+        word_search.find_unset_features_in_words([winner_dup], grammar)
+      consistent_feature_val_list =
+        consistent_feature_values(winner_dup, unset_uf_features,
+                                  grammar, test_result)
+      # Return an array of WinnerValuePackage objects for the successful
+      # features.
+      consistent_feature_val_list.map do |values|
+        WinnerValuePackage.new(winner, values)
+      end
+    end
+
+    # Returns a list of lists of feature-value pairs. Each list of
+    # feature-value pairs is such that, if all of the features in the list
+    # were set to the indicated values, then the winner would become
+    # mismatch-consistent with the grammar.
+    def consistent_feature_values(winner, unset_features, grammar,
+                                  test_result)
+      consistent_feature_val_list = []
+      # Test each unset feature, accumulate the successful (consistent) ones.
+      unset_features.each do |ufeat|
+        ufeat_val_pair = test_unset_feature(winner, ufeat, grammar,
+                                            test_result)
+        consistent_feature_val_list << [ufeat_val_pair] \
+          unless ufeat_val_pair.nil?
+      end
+      consistent_feature_val_list
+    end
+    private :consistent_feature_values
+
+    # Temporarily assigns the unset feature _ufeat_ the value that it
+    # has in the output for _winner_. It then tests to see if that
+    # makes the winner mismatch-consistent. If the resulting winner is
+    # mismatch-consistent, the feature-value pair of the unset feature
+    # and its output-derived value is returned. If the resulting winner
+    # is not mismatch-consistent, then nil is returned.
+    def test_unset_feature(winner, ufeat, grammar, test_result)
+      # Set the target feature (temporarily) to the value of its output
+      # correspondent in the winner.
+      ufeat.value = winner.out_feat_corr_of_uf(ufeat).value
+      # Collect all of the successful winners thus far observed, and
+      # add the target winner with the target feature set.
+      word_list = test_result.success_winners.dup
+      word_list << winner
+      output_list = word_list.map(&:output)
+      # If the winner is now mismatch-consistent, return the feature-value
+      # pair combining the target unset feature and its output-derived value.
+      # Return nil if the output list is inconsistent with the grammar.
+      val_pair = nil
+      if consistency_checker.mismatch_consistent?(output_list, grammar)
+        val_pair = @fv_pair_class.new(ufeat, ufeat.value)
+      end
+      # In any event, unset the temporarily set feature.
+      ufeat.unset
+      val_pair
+    end
+    private :test_unset_feature
+  end
+end
---
+++ spec/otlearn/inductive_feature_value_finder_spec.rb
@@ -0,0 +1,153 @@
+# frozen_string_literal: true
+
+# Author: Bruce Tesar
+
+require 'rspec'
+require 'otlearn/consistency_checker'
+require 'word_search'
+require 'feature_value_pair'
+require 'output'
+require 'word'
+require 'grammar'
+require 'otlearn/grammar_test_result'
+require 'otlearn/inductive_feature_value_finder'
+
+RSpec.describe OTLearn::InductiveFeatureValueFinder do
+  let(:consistency_checker) do
+    instance_double(OTLearn::ConsistencyChecker, 'consistency_checker')
+  end
+  let(:word_search) { instance_double(WordSearch, 'word_search') }
+  let(:fv_pair_class) { class_double(FeatureValuePair, 'fv_pair_class') }
+  let(:winner) { double('winner') }
+  let(:winner_dup) { double('winner_dup') }
+  let(:output) { instance_double(Output, 'output') }
+  let(:grammar) { instance_double(Grammar, 'grammar') }
+  let(:test_result) do
+    instance_double(OTLearn::GrammarTestResult, 'test_result')
+  end
+  let(:unset_feature1) { double('unset_feature1') }
+  let(:unset_feature2) { double('unset_feature2') }
+  let(:out_feature1) { double('out_feature1') }
+  let(:out_feature2) { double('out_feature2') }
+  let(:out_value1) { double('out_value1') }
+  let(:out_value2) { double('out_value2') }
+  let(:fv1) { instance_double(FeatureValuePair, 'feature_value1') }
+  let(:fv2) { instance_double(FeatureValuePair, 'feature_value2') }
+
+  before do
+    @finder = described_class.new(fv_pair_class: fv_pair_class)
+    @finder.consistency_checker = consistency_checker
+    @finder.word_search = word_search
+    allow(test_result).to receive(:success_winners).and_return([])
+    allow(fv_pair_class).to receive(:new).with(unset_feature1, out_value1)\
+                                         .and_return(fv1)
+    allow(fv_pair_class).to receive(:new).with(unset_feature2, out_value2)\
+                                         .and_return(fv2)
+    allow(winner).to receive(:output).and_return(output)
+    allow(grammar).to receive(:parse_output).with(output)\
+                                            .and_return(winner_dup)
+    allow(winner_dup).to receive(:out_feat_corr_of_uf)\
+      .with(unset_feature1).and_return(out_feature1)
+    allow(winner_dup).to receive(:out_feat_corr_of_uf)\
+      .with(unset_feature2).and_return(out_feature2)
+    allow(winner_dup).to receive(:output).and_return(output)
+    allow(out_feature1).to receive(:value).and_return(out_value1)
+    allow(out_feature2).to receive(:value).and_return(out_value2)
+    allow(unset_feature1).to receive(:value=).with(out_value1)
+    allow(unset_feature2).to receive(:value=).with(out_value2)
+    allow(unset_feature1).to receive(:value).and_return(out_value1)
+    allow(unset_feature2).to receive(:value).and_return(out_value2)
+    allow(unset_feature1).to receive(:unset)
+    allow(unset_feature2).to receive(:unset)
+  end
+
+  context 'when one feature value is successful' do
+    before do
+      allow(word_search).to receive(:find_unset_features_in_words)\
+        .with([winner_dup], grammar).and_return([unset_feature1])
+      allow(consistency_checker).to receive(:mismatch_consistent?)\
+        .with([output], grammar).and_return(true)
+      @result = @finder.run(winner, grammar, test_result)
+    end
+
+    it 'returns a list with exactly one success set' do
+      expect(@result.size).to eq 1
+    end
+
+    it 'returns the successful feature value' do
+      expect(@result[0].values).to contain_exactly(fv1)
+    end
+
+    it 'returns the failed winner' do
+      expect(@result[0].winner).to eq winner
+    end
+  end
+
+  context 'when one feature value is unsuccessful' do
+    before do
+      allow(word_search).to receive(:find_unset_features_in_words)\
+        .with([winner_dup], grammar).and_return([unset_feature1])
+      allow(consistency_checker).to receive(:mismatch_consistent?)\
+        .with([output], grammar).and_return(false)
+      @result = @finder.run(winner, grammar, test_result)
+    end
+
+    it 'returns a list with no success sets' do
+      expect(@result).to be_empty
+    end
+  end
+
+  context 'when one of two feature values is successful' do
+    before do
+      allow(word_search).to receive(:find_unset_features_in_words)\
+        .with([winner_dup], grammar)\
+        .and_return([unset_feature1, unset_feature2])
+      allow(consistency_checker).to receive(:mismatch_consistent?)\
+        .with([output], grammar).and_return(false, true)
+      @result = @finder.run(winner, grammar, test_result)
+    end
+
+    it 'returns a list with exactly one success set' do
+      expect(@result.size).to eq 1
+    end
+
+    it 'returns the successful feature value' do
+      expect(@result[0].values).to contain_exactly(fv2)
+    end
+
+    it 'returns the failed winner' do
+      expect(@result[0].winner).to eq winner
+    end
+  end
+
+  context 'when two feature values are successful' do
+    before do
+      allow(word_search).to receive(:find_unset_features_in_words)\
+        .with([winner_dup], grammar)\
+        .and_return([unset_feature1, unset_feature2])
+      allow(consistency_checker).to receive(:mismatch_consistent?)\
+        .with([output], grammar).and_return(true, true)
+      @result = @finder.run(winner, grammar, test_result)
+    end
+
+    it 'returns a list with exactly two success sets' do
+      expect(@result.size).to eq 2
+    end
+
+    it 'returns the first successful feature value' do
+      expect(@result[0].values).to contain_exactly(fv1)
+    end
+
+    it 'returns the failed winner for the first feature' do
+      expect(@result[0].winner).to eq winner
+    end
+
+    it 'returns the second successful feature value' do
+      expect(@result[1].values).to contain_exactly(fv2)
+    end
+
+    it 'returns the failed winner for the second feature' do
+      expect(@result[1].winner).to eq winner
+    end
+  end
+end
