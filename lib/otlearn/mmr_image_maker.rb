a9983aa16291025aae999b8a1610a65181f48903
Bruce Tesar <tesar@rutgers.edu>
2021-06-01 00:52:39 -0400

    MmrImageMaker now lists all of the failed winners, in addition to showing the one that was actually chosen.

--- lib/otlearn/mmr_image_maker.rb
+++ lib/otlearn/mmr_image_maker.rb
@@ -26,6 +26,7 @@ module OTLearn
       # indicate if the grammar was changed
       sheet[2, 1] = "Grammar Changed: #{mmr_step.changed?.to_s.upcase}"
       add_failed_winner_info(mmr_step, sheet)
+      add_all_failed_winners(mmr_step, sheet)
       sheet
     end
 
@@ -34,11 +35,27 @@ module OTLearn
       failed_winner = step.failed_winner
       subsheet = @sheet_class.new
       subsheet[1, 2] = 'Failed Winner'
-      subsheet[1, 3] = failed_winner.morphword.to_s
-      subsheet[1, 4] = failed_winner.input.to_s
-      subsheet[1, 5] = failed_winner.output.to_s
+      word_to_row(failed_winner, 1, subsheet)
       sheet.append(subsheet)
     end
     private :add_failed_winner_info
+
+    def word_to_row(word, row, sheet)
+      sheet[row, 3] = word.morphword.to_s
+      sheet[row, 4] = word.input.to_s
+      sheet[row, 5] = word.output.to_s
+      sheet
+    end
+    private :word_to_row
+
+    def add_all_failed_winners(step, sheet)
+      subsheet = @sheet_class.new
+      subsheet[1, 2] = 'All Failed Winners'
+      step.failed_winner_list.each_with_index do |fw, idx|
+        word_to_row(fw, 2 + idx, subsheet)
+      end
+      sheet.append(subsheet)
+    end
+    private :add_all_failed_winners
   end
 end
--- spec/otlearn/mmr_image_maker_spec.rb
+++ spec/otlearn/mmr_image_maker_spec.rb
@@ -2,35 +2,51 @@
 
 # Author: Bruce Tesar
 
+require 'rspec'
 require 'otlearn/mmr_image_maker'
+require 'sheet'
 
 RSpec.describe OTLearn::MmrImageMaker do
   let(:mmr_step) { double('mmr_step') }
   let(:failed_winner) { double('failed_winner') }
+  let(:winner1) { double('winner1') }
   let(:sheet_class) { double('sheet_class') }
-  let(:sheet) { double('sheet') }
-  let(:subsheet) { double('subsheet') }
+  let(:sheet) { instance_double(Sheet, 'sheet') }
+  let(:subsheet_chosen) { instance_double(Sheet, 'subsheet_chosen') }
+  let(:subsheet_all) { instance_double(Sheet, 'subsheet_all') }
+  let(:failed_winner_morphword) { 'failed_winner_morphword' }
+  let(:failed_winner_input) { 'failed_winner_input' }
+  let(:failed_winner_output) { 'failed_winner_output' }
+  let(:winner1_morphword) { 'winner1_morphword' }
+  let(:winner1_input) { 'winner1_input' }
+  let(:winner1_output) { 'winner1_output' }
 
   before do
-    allow(sheet_class).to receive(:new).and_return(sheet, subsheet)
+    allow(sheet_class).to receive(:new).and_return(sheet, subsheet_chosen,
+                                                   subsheet_all)
     allow(sheet).to receive(:[]=)
     allow(sheet).to receive(:append)
-    allow(subsheet).to receive(:[]=)
+    allow(subsheet_chosen).to receive(:[]=)
+    allow(subsheet_all).to receive(:[]=)
     @mmr_image_maker = described_class.new(sheet_class: sheet_class)
+    allow(failed_winner).to\
+      receive(:morphword).and_return(failed_winner_morphword)
+    allow(failed_winner).to receive(:input).and_return(failed_winner_input)
+    allow(failed_winner).to receive(:output).and_return(failed_winner_output)
+    allow(winner1).to\
+      receive(:morphword).and_return(winner1_morphword)
+    allow(winner1).to receive(:input).and_return(winner1_input)
+    allow(winner1).to receive(:output).and_return(winner1_output)
   end
 
-  context 'with an MMR step with one failing winner adopted' do
-    let(:failed_winner_morphword) { 'failed_winner_morphword' }
-    let(:failed_winner_input) { 'failed_winner_input' }
-    let(:failed_winner_output) { 'failed_winner_output' }
+  context 'with an MMR step with a failing winner adopted' do
+    let(:failed_winner_list) { [winner1, failed_winner] }
 
     before do
       allow(mmr_step).to receive(:changed?).and_return(true)
       allow(mmr_step).to receive(:failed_winner).and_return(failed_winner)
-      allow(failed_winner).to\
-        receive(:morphword).and_return(failed_winner_morphword)
-      allow(failed_winner).to receive(:input).and_return(failed_winner_input)
-      allow(failed_winner).to receive(:output).and_return(failed_winner_output)
+      allow(mmr_step).to receive(:failed_winner_list)\
+        .and_return(failed_winner_list)
       @mmr_image = @mmr_image_maker.get_image(mmr_step)
     end
 
@@ -44,38 +60,73 @@ RSpec.describe OTLearn::MmrImageMaker do
         have_received(:[]=).with(2, 1, 'Grammar Changed: TRUE')
     end
 
-    it 'appends the subsheet' do
-      expect(@mmr_image).to have_received(:append).with(subsheet)
+    it 'appends the chosen winner subsheet' do
+      expect(@mmr_image).to have_received(:append).with(subsheet_chosen)
     end
 
     it 'indicates the morphword of the failed winner used' do
-      expect(subsheet).to\
+      expect(subsheet_chosen).to\
         have_received(:[]=).with(1, 3, failed_winner_morphword)
     end
 
     it 'indicates the input of the failed winner used' do
-      expect(subsheet).to\
+      expect(subsheet_chosen).to\
         have_received(:[]=).with(1, 4, failed_winner_input)
     end
 
     it 'indicates the output of the failed winner used' do
-      expect(subsheet).to\
+      expect(subsheet_chosen).to\
         have_received(:[]=).with(1, 5, failed_winner_output)
+    end
+
+    it 'appends the all winners subsheet' do
+      expect(@mmr_image).to have_received(:append).with(subsheet_all)
+    end
+
+    it 'adds the heading to the winners subsheet' do
+      expect(subsheet_all).to\
+        have_received(:[]=).with(1, 2, 'All Failed Winners')
+    end
+
+    it 'indicates the morphword of winner1' do
+      expect(subsheet_all).to\
+        have_received(:[]=).with(2, 3, winner1_morphword)
+    end
+
+    it 'indicates the input of winner1' do
+      expect(subsheet_all).to\
+        have_received(:[]=).with(2, 4, winner1_input)
+    end
+
+    it 'indicates the output of winner1' do
+      expect(subsheet_all).to\
+        have_received(:[]=).with(2, 5, winner1_output)
+    end
+
+    it 'indicates the morphword of winner2' do
+      expect(subsheet_all).to\
+        have_received(:[]=).with(3, 3, failed_winner_morphword)
+    end
+
+    it 'indicates the input of winner2' do
+      expect(subsheet_all).to\
+        have_received(:[]=).with(3, 4, failed_winner_input)
+    end
+
+    it 'indicates the output of winner2' do
+      expect(subsheet_all).to\
+        have_received(:[]=).with(3, 5, failed_winner_output)
     end
   end
 
   context 'with an MMR step without a newly set feature' do
-    let(:failed_winner_morphword) { 'failed_winner_morphword' }
-    let(:failed_winner_input) { 'failed_winner_input' }
-    let(:failed_winner_output) { 'failed_winner_output' }
+    let(:failed_winner_list) { [failed_winner] }
 
     before do
       allow(mmr_step).to receive(:changed?).and_return(false)
       allow(mmr_step).to receive(:failed_winner).and_return(failed_winner)
-      allow(failed_winner).to\
-        receive(:morphword).and_return(failed_winner_morphword)
-      allow(failed_winner).to receive(:input).and_return(failed_winner_input)
-      allow(failed_winner).to receive(:output).and_return(failed_winner_output)
+      allow(mmr_step).to receive(:failed_winner_list)\
+        .and_return(failed_winner_list)
       @mmr_image = @mmr_image_maker.get_image(mmr_step)
     end
 
@@ -89,22 +140,22 @@ RSpec.describe OTLearn::MmrImageMaker do
         have_received(:[]=).with(2, 1, 'Grammar Changed: FALSE')
     end
 
-    it 'appends the subsheet' do
-      expect(@mmr_image).to have_received(:append).with(subsheet)
+    it 'appends the chosen winner subsheet' do
+      expect(@mmr_image).to have_received(:append).with(subsheet_chosen)
     end
 
     it 'indicates the morphword of the failed winner used' do
-      expect(subsheet).to\
+      expect(subsheet_chosen).to\
         have_received(:[]=).with(1, 3, failed_winner_morphword)
     end
 
     it 'indicates the input of the failed winner used' do
-      expect(subsheet).to\
+      expect(subsheet_chosen).to\
         have_received(:[]=).with(1, 4, failed_winner_input)
     end
 
     it 'indicates the output of the failed winner used' do
-      expect(subsheet).to\
+      expect(subsheet_chosen).to\
         have_received(:[]=).with(1, 5, failed_winner_output)
     end
   end
