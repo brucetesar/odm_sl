30e1a23b6fe718854fb786ef5e9e219282fd8247
Bruce Tesar <tesar@rutgers.edu>
2021-07-25 20:37:16 -0400

    Internally refactored PAS::Gen.

--- lib/pas/gen.rb
+++ lib/pas/gen.rb
@@ -17,62 +17,84 @@ module PAS
       @system = system
     end
 
+    # Returns an array of all the candidates for _input_.
+    # :call-seq:
+    #   run(input) -> array
     def run(input)
-      start_rep = Word.new(@system, input) # full input, but empty output, io_corr
+      # full input, but empty output, io_corr
+      start_rep = Word.new(@system, input)
       start_rep.output.morphword = input.morphword
       # create two lists of partial candidates, distinguished by whether or
       # not they contain a syllable with main stress.
-      no_stress_yet = [start_rep]
-      main_stress_assigned = []
-
+      no_stress = [start_rep]
+      main_stressed = []
       # for each input segment, add it to the output in all possible ways,
       # creating new partial candidates
       input.each do |isyl|
-        # copy the partial candidate lists to old_*, reset the lists to empty.
-        old_no_stress_yet = no_stress_yet
-        old_main_stress_assigned = main_stress_assigned
-        no_stress_yet = []
-        main_stress_assigned = []
-        # iterate over old_no_stress_yet, for each member create a new candidate
-        # for each of the ways of adding the next syllable.
-        old_no_stress_yet.each do |w|
-          no_stress_yet <<
-            extend_word_output(w, isyl) { |s| s.set_unstressed.set_short }
-          main_stress_assigned <<
-            extend_word_output(w, isyl) { |s| s.set_main_stress.set_short }
-          no_stress_yet <<
-            extend_word_output(w, isyl) { |s| s.set_unstressed.set_long }
-          main_stress_assigned <<
-            extend_word_output(w, isyl) { |s| s.set_main_stress.set_long }
-        end
-        # iterate over old_main_stress_assigned, for each member create
-        # a new candidate for each of the ways of adding the next syllable.
-        old_main_stress_assigned.each do |w|
-          main_stress_assigned <<
-            extend_word_output(w, isyl) { |s| s.set_unstressed.set_short }
-          main_stress_assigned <<
-            extend_word_output(w, isyl) { |s| s.set_unstressed.set_long }
-        end
+        # copy the lists to old_*, reset the lists to empty.
+        old_no_stress = no_stress
+        old_main_stressed = main_stressed
+        no_stress = []
+        main_stressed = []
+        # iterate over old_no_stress, for each member create a new
+        # candidate for each of the ways of adding the next syllable.
+        extend_no_stress(isyl, old_no_stress, no_stress, main_stressed)
+        # iterate over old_main_stressed, for each member create a new
+        # candidate for each of the ways of adding the next syllable.
+        extend_stressed(isyl, old_main_stressed, main_stressed)
       end
       # Combine candidates with and without a main stress
-      candidates = main_stress_assigned + no_stress_yet
+      candidates = main_stressed + no_stress
       # Set the constraint violations for each candidate, return array.
       candidates.each(&:eval)
     end
 
-    # Takes a word partial description (full input, partial output), along with
-    # a reference to the next input syllable to have a correspondent added
-    # to the output. A copy of the word, containing the new output syllable
-    # as an output correspondent to the input syllable, is returned.
+    # Incorporates the next input syllable _isyl_ by adding it onto
+    # partial candidates that do not yet have a main stress assigned.
+    # Returns nil.
+    def extend_no_stress(isyl, base_reps, no_stress, main_stressed)
+      base_reps.each do |word|
+        no_stress <<
+          extend_word_output(word, isyl) { |s| s.set_unstressed.set_short }
+        main_stressed <<
+          extend_word_output(word, isyl) { |s| s.set_main_stress.set_short }
+        no_stress <<
+          extend_word_output(word, isyl) { |s| s.set_unstressed.set_long }
+        main_stressed <<
+          extend_word_output(word, isyl) { |s| s.set_main_stress.set_long }
+      end
+      nil
+    end
+    private :extend_no_stress
+
+    # Incorporates the next input syllable _isyl_ by adding it onto
+    # partial candidates that already have a main stress assigned.
+    # Returns nil.
+    def extend_stressed(isyl, base_reps, main_stressed)
+      base_reps.each do |word|
+        main_stressed <<
+          extend_word_output(word, isyl) { |s| s.set_unstressed.set_short }
+        main_stressed <<
+          extend_word_output(word, isyl) { |s| s.set_unstressed.set_long }
+      end
+      nil
+    end
+    private :extend_stressed
+
+    # Takes a word partial description (full input, partial output), along
+    # with a reference to the next input syllable to have a correspondent
+    # added to the output. A copy of the word, containing the new output
+    # syllable as an output correspondent to the input syllable, is
+    # returned.
     #
-    # The new output syllable is formed by duplicating
-    # the input syllable (to copy morpheme info, etc.), and then the output
-    # syllable is passed to the block parameter, which sets the feature values
-    # for the new output syllable. The new output syllable is added to the end
-    # of the output, and a new IO correspondence pair is added.
+    # The new output syllable is formed by duplicating the input syllable
+    # (to copy morpheme info, etc.), and then the output syllable is passed
+    # to the block parameter, which sets the feature values for the new
+    # output syllable. The new output syllable is added to the end of
+    # the output, and a new IO correspondence pair is added.
     def extend_word_output(word, in_syl)
       new_w = word.dup_for_gen
-      out_syl = yield(in_syl.dup) # block sets features of new output syllable.
+      out_syl = yield(in_syl.dup) # block sets features of new output syl.
       new_w.output << out_syl
       new_w.add_to_io_corr(in_syl, out_syl)
       new_w
