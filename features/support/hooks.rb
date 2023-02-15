85c0ccac0db89951754ae02342afc3f66a6be8d4
Bruce Tesar <tesar@rutgers.edu>
2023-02-15 00:12:21 -0500

    The cucumber Before hook now sets the current directory to the cltester working directory in temp, making it the default working directory for every scenario.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -7,9 +7,10 @@ Before do
   lib_dir = File.expand_path('../..', __dir__)
   @cltester_dir = File.expand_path('temp/cltester', lib_dir)
   Dir.mkdir(@cltester_dir) unless Dir.exist?(@cltester_dir)
+  # Change to the temporary directory
+  Dir.chdir(@cltester_dir)
 end
 
 After do
   # Delete any files and subdirectories remaining in the cltester dir.
-  puts "After hook: #{@cltester_dir}"
 end
