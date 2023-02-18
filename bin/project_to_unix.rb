875d767a7bb80320426d5952b00057295facfa8c
Bruce Tesar <tesar@rutgers.edu>
2023-02-17 23:38:58 -0500

    Added a few additional exclusions from the generated unix version, and addressed a few rubocop suggestions.

--- bin/project_to_unix.rb
+++ bin/project_to_unix.rb
@@ -42,26 +42,29 @@ Find.find(SOURCE) do |path|
   Find.prune if File.basename(path) == 'temp'
   Find.prune if File.basename(path) == 'test'
   Find.prune if File.basename(path) == 'tmp'
+  Find.prune if File.basename(path) == '.gitattributes'
   Find.prune if File.basename(path) == '.gitignore'
   Find.prune if File.basename(path) == '.rspec'
   Find.prune if File.basename(path) == '.rspec_status'
+  Find.prune if File.basename(path) == '.rubocop.yml'
+  Find.prune if File.basename(path) == 'cucumber.yml'
   Find.prune if File.basename(path) == 'LICENSE'
-  Find.prune if File.basename(path) == 'README'
   Find.prune if File.basename(path) == 'Rakefile'
+  Find.prune if File.basename(path) == 'README'
   Find.prune if File.basename(path) =~ /[.]bat$/
-  Find.prune if path =~ /bin\/main\.rb$/
+  Find.prune if path =~ %r{bin/main\.rb$}
   # Translate the path to the target
   target_path = path.gsub(SOURCE, TARGET)
   if File.directory?(path)
     # If a directory, a target counterpart should be created.
-    Dir.mkdir(target_path) unless Dir.exist?(target_path)
+    FileUtils.mkdir_p(target_path)
   elsif File.file?(path)
     # If neither a directory nor a file, don't copy it.
     # Read the file in binary mode into an ASCII-8BIT string.
-    content_string = IO.read(path, mode: 'rb')
+    content_string = File.read(path, mode: 'rb')
     # Transcode the string into unix line terminator format.
     trans_string = string2unix(content_string)
     # Write the transcoded string to the target file.
-    IO.write(target_path, trans_string, mode: 'wb')
+    File.write(target_path, trans_string, mode: 'wb')
   end
 end
